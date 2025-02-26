77cda1f34664d334d089f882c363d97a198df47a
13750775 <change5248@gmail.com>
2025-02-26 12:23:16 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,16 @@
+/// week01-1 瘋狂程設 兩數相加
+///file-new empty 開啟新檔案
+///ctrl 存檔成 wweek01-1.cpp 要記得加.cpp
+///下周考試寫這題，不用寫藍色的中文註解
+#include<stdio.h>///檔名不能寫錯
+///studle 的意思是 標準的standard ipnut/output
+///有了 stdio.h的外掛，才能用 scanf() printf()
+
+int main()
+{
+	int a, b;///整數a,b
+	scanf( "%d" , &a);
+	scanf( "%d" , &b);
+
+	printf("%d",a+b);
+}
